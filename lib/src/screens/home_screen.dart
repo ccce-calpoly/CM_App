da7ba8cfe5852fd4ea49010292e61337c3280228
lamkin1 <jamesclamkin@gmail.com>
2024-05-07 16:10:44 -0700

    first calendar workings

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,79 +1,133 @@
+import 'package:ccce_application/src/collections/calevent.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'dart:collection';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-  final String title = 'CM Home';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+Future<HashMap<DateTime, List<CalEvent>>> fetchEvents() async {
+  final snapshot = await FirebaseFirestore.instance.collection('events').get();
+  HashMap<DateTime, List<CalEvent>> events = HashMap();
+
+  for (final doc in snapshot.docs) {
+    final event = CalEvent.fromSnapshot(doc);
+    var start = event.startTime;
+    final date = DateTime.utc(
+      start.year,
+      start.month,
+      start.day,
+    ); // Format date as YYYY-MM-DD
+
+    events.update(date, (value) {
+      value.add(event);
+      return value;
+    }, ifAbsent: () => <CalEvent>[event]);
+  }
+
+  return events;
+}
+
+class _CalendarScreenState extends State<HomeScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  late HashMap<DateTime, List<CalEvent>> eventMap = HashMap();
+  List _selectedEvents = [];
+
   static const calPolyGreen = Color(0xFF003831);
   static const appBackgroundColor = Color(0xFFE4E3D3);
 
   @override
+  void initState() {
+    super.initState();
+    fetchEvents().then((events) {
+      setState(() {
+        eventMap = events;
+      });
+    });
+  }
+
+  void updateEventsForDay(DateTime selectedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = selectedDay;
+      //print(selectedDay);
+      _selectedEvents = _eventLoader(selectedDay);
+    });
+  }
+
+  List<CalEvent> _eventLoader(DateTime day) {
+    final events = _getEventsForDay(day);
+    //print('${day} ${events}');
+    return events;
+  }
+
+  var events = fetchEvents();
+
+  List<CalEvent> _getEventsForDay(DateTime day) {
+    return eventMap.putIfAbsent(day, () => <CalEvent>[]);
+    //return events.values.expand((list) => list).where((event) => isSameDay(event.startTime, day)).toList();
+  }
+
+  Widget buildCalendar() {
+    return TableCalendar<CalEvent>(
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: _focusedDay,
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        updateEventsForDay(selectedDay);
+      },
+      onFormatChanged: (format) {
+        if (_calendarFormat != format) {
+          // Call `setState()` when updating calendar format
+          setState(() {
+            _calendarFormat = format;
+          });
+        }
+      },
+      onPageChanged: (focusedDay) {
+        // No need to call `setState()` here
+        _focusedDay = focusedDay;
+      },
+      eventLoader: (day) => _eventLoader(day),
+    );
+  }
+
+  Widget buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: ListTile(
+                  title: Text(event.toString()),
+                  //onTap: () => print('$event tapped!'),
+                ),
+              ))
+          .toList(),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Home Page - Implement Calendar'));
-    // drawer: Drawer(
-    //     child: ListView(children: <Widget>[
-    //   ListTile(
-    //     title: const Text('Home'),
-    //     onTap: () {
-    //       Navigator.pushReplacement(context,
-    //           MaterialPageRoute(builder: (context) => const HomeScreen()));
-    //     },
-    //   ),
-    //   ListTile(
-    //     title: const Text('Events'),
-    //     onTap: () {
-    //       Navigator.pushReplacement(context,
-    //           MaterialPageRoute(builder: (context) => const HomeScreen()));
-    //     },
-    //   ),
-    //   ListTile(
-    //     title: const Text('Club Info'),
-    //     onTap: () {
-    //       Navigator.pushReplacement(context,
-    //           MaterialPageRoute(builder: (context) => const HomeScreen()));
-    //     },
-    //   ),
-    //   ListTile(
-    //     title: const Text('Academics'),
-    //     onTap: () {
-    //       Navigator.pushReplacement(context,
-    //           MaterialPageRoute(builder: (context) => const HomeScreen()));
-    //     },
-    //   ),
-    //   ListTile(
-    //     title: const Text('Resources'),
-    //     onTap: () {
-    //       Navigator.pushReplacement(context,
-    //           MaterialPageRoute(builder: (context) => const HomeScreen()));
-    //     },
-    //   )
-    // ])),
-    /*appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-              onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: const Icon(Icons.dehaze)),
-        ),
-        backgroundColor: calPolyGreen,
-        centerTitle: true,
-        iconTheme: const IconThemeData(color: Colors.white),
-        title: Text(
-          title,
-          style: const TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.account_circle_rounded),
-            tooltip: 'Open Profile',
-            onPressed: () {
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ProfileScreen()));
-            },
-          ),
-        ],
-      ),*/
+    return Scaffold(
+        body: Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
+      buildCalendar(),
+      Expanded(
+        child: buildEventList(),
+      )
+    ]));
   }
 }
