aea3d098ff6e8324b1b2d2bd91059ec46a132edc
lamkin1 <jamesclamkin@gmail.com>
2024-05-21 14:26:59 -0700

    hot fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:ccce_application/src/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
-import 'auth_gate.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
